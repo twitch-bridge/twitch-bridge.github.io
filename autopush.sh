1ec5ad3a69e561e530c1c848704692681e71c19b
twitch-bridge <cds8q@virginia.edu>
2020-09-29 17:31:20 -0400

    set up .gitignore

--- autopush.sh
+++
@@ -1,4 +0,0 @@
-cd /Users/ds65/Desktop/Bridge/bfc2/twitch-bridge.github.io
-/usr/bin/git add .
-/usr/bin/git commit -m "auto results push"
-/usr/bin/git push
--- makehtml.R
+++
@@ -1,5 +0,0 @@
-Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
-
-setwd("/Users/ds65/Desktop/Bridge/bfc2/twitch-bridge.github.io/")
-
-rmarkdown::render("index.Rmd")
